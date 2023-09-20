14cb872e37eccaeafafe8a7167cf9d996b2fb78e
Johan Lindh <johan@linkdata.se>
2023-09-20 11:29:53 +0200

    wip

--- car.go
+++ car.go
@@ -18,12 +18,43 @@ func (ct *CarsTable) JawsContains(rq *jaws.Request) (tl []jaws.UI) {
 	return tl
 }
 
+func (ct *CarsTable) JawsClick(e *jaws.Element, name string) error {
+	switch name {
+	case "add":
+		AddRandomCar()
+		e.Jaws.Dirty(globals.CarsTable)
+	}
+	return nil
+}
+
 type Car struct {
 	VIN       string
 	Make      string
 	Model     string
 	Year      int
 	condition atomic.Value
+}
+
+var carMakes = []string{"Dodge", "Hyundai", "Acura", "Volvo", "Saab", "Lada", "Mazda"}
+var carModels = []string{"Sedan", "Coupe", "SUV", "Truck", "Cabriolet"}
+
+func AddRandomCar() {
+	var vin []byte
+	for i := 0; i < 17; i++ {
+		n := byte(rand.Intn(26 + 10))
+		if n < 10 {
+			vin = append(vin, '0'+n)
+		} else {
+			vin = append(vin, 'A'+(n-10))
+		}
+	}
+	car := &Car{
+		VIN:   string(vin),
+		Make:  carMakes[rand.Intn(len(carMakes))],
+		Model: carModels[rand.Intn(len(carModels))],
+		Year:  1970 + rand.Intn(30),
+	}
+	globals.Cars = append(globals.Cars, car)
 }
 
 func (c *Car) JawsClick(e *jaws.Element, name string) error {
@@ -34,9 +65,6 @@ func (c *Car) JawsClick(e *jaws.Element, name string) error {
 		jaws.ListMove(globals.Cars, c, 1)
 	case "remove":
 		globals.Cars = jaws.ListRemove(globals.Cars, c)
-		// the list on index.html doesn't use $.Container,
-		// so we need to remove manually
-		e.Jaws.Remove(e.MakeTemplate("car_row.html", c))
 	case "+":
 		oldVal := c.condition.Load().(int)
 		if oldVal > 99 {
@@ -57,16 +85,7 @@ func (c *Car) JawsClick(e *jaws.Element, name string) error {
 		return nil
 	}
 
-	// cars.html uses $.Container, so all we need to do is mark it dirty
 	e.Jaws.Dirty(globals.CarsTable)
-
-	// the list on index.html doesn't use $.Container,
-	// so we need to reorder that manually
-	var ordering []interface{}
-	for _, t := range globals.CarsTable.JawsContains(e.Request) {
-		ordering = append(ordering, t)
-	}
-	e.Jaws.Order(ordering)
 	return nil
 }
 
