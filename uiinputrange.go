8d734b48ce55435c68e04a91978c09abf232ac0f
Johan Lindh <johan@linkdata.se>
2023-10-05 13:27:54 +0200

    wip

--- uiinputrange.go
+++ uiinputrange.go
@@ -3,38 +3,25 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"io"
 
 	"github.com/linkdata/jaws"
 )
 
 type uiInputRange struct{ *Globals }
 
-var _ jaws.UI = (*uiInputRange)(nil)          // statically ensure we implement this interface
 var _ jaws.FloatGetter = (*uiInputRange)(nil) // statically ensure we implement this interface
+var _ jaws.HtmlGetter = (*uiInputRange)(nil)  // statically ensure we implement this interface
 
-func (ui uiInputRange) JawsRender(e *jaws.Element, w io.Writer, params []interface{}) {
-	jaws.NewUiSpan(ui).JawsRender(e, w, params)
-	e.Dirty(ui)
-}
-
-func (ui uiInputRange) JawsUpdate(e *jaws.Element) {
+func (ui uiInputRange) JawsGetHtml(e *jaws.Element) (v template.HTML) {
 	ui.mu.RLock()
-	val := ui.inputRange
-	ui.mu.RUnlock()
 	switch {
-	case val < 50:
+	case ui.inputRange < 50:
 		e.SetAttr("style", "color:red")
 	case ui.inputRange < 90:
 		e.RemoveAttr("style")
 	default:
 		e.SetAttr("style", "color:green")
 	}
-	jaws.NewUiSpan(ui).JawsUpdate(e)
-}
-
-func (ui uiInputRange) JawsGetHtml(e *jaws.Element) (v template.HTML) {
-	ui.mu.RLock()
 	v = template.HTML(fmt.Sprint(ui.inputRange))
 	ui.mu.RUnlock()
 	return
