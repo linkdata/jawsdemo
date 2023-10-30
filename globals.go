192eb6884287e788f74f6c4bfaf365973a94d2e2
Johan Lindh <johan@linkdata.se>
2023-10-30 12:34:03 +0100

    jaws v0.40.0

--- globals.go
+++ globals.go
@@ -66,10 +66,10 @@ func (g *Globals) JawsClick(e *jaws.Element, name string) error {
 	defer g.mu.Unlock()
 	if g.inputButton == "Meh" {
 		g.inputButton = "Woo"
-		e.Jaws().SetAttr(g.InputButton(), "disabled", "")
+		e.Jaws.SetAttr(g.InputButton(), "disabled", "")
 	} else {
 		g.inputButton = "Meh"
-		e.Jaws().RemoveAttr(g.InputButton(), "disabled")
+		e.Jaws.RemoveAttr(g.InputButton(), "disabled")
 	}
 	e.Dirty(g.InputButton())
 	return nil
