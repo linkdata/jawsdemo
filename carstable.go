49ddfad7606dfaf528bf7429b01d3355900fbd38
Johan Lindh <johan@linkdata.se>
2024-02-12 12:49:41 +0100

    jaws v0.49.0

--- carstable.go
+++ carstable.go
@@ -4,11 +4,11 @@ import "github.com/linkdata/jaws"
 
 type CarsTable struct{}
 
-func (ct *CarsTable) JawsContains(rq *jaws.Request) (tl []jaws.UI) {
+func (ct *CarsTable) JawsContains(e *jaws.Element) (tl []jaws.UI) {
 	for _, c := range globals.Cars {
-		tl = append(tl, rq.MakeTemplate("car_row.html", c))
+		tl = append(tl, e.MakeTemplate("car_row.html", c))
 	}
-	tl = append(tl, rq.MakeTemplate("car_row.html", nil))
+	tl = append(tl, e.MakeTemplate("car_row.html", nil))
 	return tl
 }
 
